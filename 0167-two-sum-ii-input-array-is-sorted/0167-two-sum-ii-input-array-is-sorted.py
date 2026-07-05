4911ac1594e33633c8ad78f04db8efa7723840d7
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-05 22:02:46 +0530

    Time: 11 ms (12.68%), Space: 12.9 MB (43.47%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -9,7 +9,7 @@ class Solution(object):
         j=len(numbers)-1
         while(i<j):
             if numbers[i]+numbers[j]==target:
-                a=[i+1,j+1]
+                return [i+1,j+1]
                 break
             elif numbers[i]+numbers[j]<target:
                 i+=1
@@ -17,4 +17,3 @@ class Solution(object):
             else :
                 j-=1
                 continue
-        return a
