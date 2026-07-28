2f1b2f761fe368f2618ab554958c1474ad36db5f
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-28 14:37:02 +0530

    Time: 3 ms (49.12%), Space: 12.4 MB (18%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def square(self,n):
+        sum=0
+        while(n!=0):
+            sum=sum+(n%10)**2
+            n=n/10
+        return sum
+    
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        slow=n
+        fast=n
+        while (fast!=1):
+            slow=self.square(slow)
+            fast=self.square(fast)
+            fast=self.square(fast)
+            if slow==fast and slow!=1:
+                return False
+        return True
+
+        
