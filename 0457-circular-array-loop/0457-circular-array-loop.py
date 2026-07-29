8a8c7d0b68525754c7b31d09e97f2e09942cf58e
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-29 11:30:26 +0530

    Time: 537 ms (5.56%), Space: 12.4 MB (81.25%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.py
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -17,26 +17,19 @@ class Solution(object):
             slow, fast = i, i
             
             while True:
-                # TODO: move slow one step
+                
                 slow=next_index(slow)
-                # TODO: check direction consistency for slow's move
-                # if direction breaks: mark this path visited, break out to outer loop
                 if  not is_same_direction(slow, next_index(slow)):
                     nums[slow]=0
                     break
-                
-                # TODO: move fast two steps (check direction on EACH hop)
-                # if direction breaks at either hop: mark this path visited, break
                 fast=next_index(next_index(fast))
                 if not is_same_direction(fast,next_index(fast)) or not is_same_direction(next_index(fast),next_index(next_index(fast))):
                     nums[fast]=0
                     break
                 if slow == fast:
-                    break  # potential cycle - or self loop, need to check
+                    break  
             
-            # TODO: after loop - check if it was a real cycle (length > 1) or self-loop
-            # if real cycle found: return True
-            # if not: mark all nodes on this path as visited (set to 0)
+           
             if slow==fast and fast!=next_index(slow):
                 return True
         
