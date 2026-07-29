a7b1d1489770351d56f53ad6552328b7f1053adb
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-29 18:06:55 +0530

    Time: 597 ms (10.5%), Space: 16.9 MB (5.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -9,7 +9,6 @@ class Solution(object):
         res=0
         for high in range(len(s)):
             freq[s[high]]=freq.get(s[high],0)+1
-            k=high-low+1
             while freq[s[high]] > 1:
                 freq[s[low]]-=1
                 if freq[s[low]]==0:
