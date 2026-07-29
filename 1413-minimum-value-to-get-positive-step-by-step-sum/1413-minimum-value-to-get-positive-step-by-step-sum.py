22ecedd74e11eb5f0798004d22030ef15c2b4575
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-29 12:33:14 +0530

    Time: 0 ms (100%), Space: 12.3 MB (84.21%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -7,5 +7,6 @@ class Solution(object):
         # sum=[0]*(len(nums))
         for i in range(1,len(nums)):
             nums[i]=nums[i]+nums[i-1]
-        return (min(nums)*(-1))+1 if min(nums)<1 else 1
+        x=min(nums)
+        return (x*(-1))+1 if x<1 else 1
         
