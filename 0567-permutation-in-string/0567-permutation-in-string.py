2751ed5e853b9f526b9be8d2d7193af060b88925
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-30 17:40:23 +0530

    Time: 31 ms (47.39%), Space: 12.7 MB (46.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,37 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        have=dict()
-        needed=dict()
-        for i in s1:
-            needed[i]=needed.get(i,0)+1
-        required=len(needed)
-        formed=0
-        high=low=0
-        while high<len(s1)+low and high<len(s2):
-            have[s2[high]]=have.get(s2[high],0)+1
-            if s2[high] in have and have[s2[high]]==needed.get(s2[high],0):
-                formed+=1
-            if formed==required:
+        if len(s1) > len(s2):
+            return False
+
+        needed = dict()
+        for c in s1:
+            needed[c] = needed.get(c, 0) + 1
+        required = len(needed)
+
+        have = dict()
+        formed = 0
+        low = 0
+
+        for high in range(len(s2)):
+            c = s2[high]
+            have[c] = have.get(c, 0) + 1
+            if c in needed:
+                if have[c] == needed[c]:
+                    formed += 1
+                elif have[c] == needed[c] + 1:
+                    formed -= 1
+
+            if high >= len(s1):
+                d = s2[low]
+                if d in needed:
+                    if have[d] == needed[d]:
+                        formed -= 1
+                    elif have[d] == needed[d] + 1:
+                        formed += 1
+                have[d] -= 1
+                low += 1
+
+            if formed == required:
                 return True
-            else:
-                if high==(low+len(s1)-1):
-                    
-                    if s2[low] in have and have[s2[low]]==needed.get(s2[low],0):
-                        formed-=1
-                    have[s2[low]]-=1
-                    low+=1
-            high+=1
+
         return False
