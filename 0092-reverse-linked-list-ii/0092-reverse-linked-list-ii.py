f401ce8859d4f0a4e23dc7d7a6d1e0b556aef405
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-30 15:15:41 +0530

    Time: 0 ms (100%), Space: 12.5 MB (79.37%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,44 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def reverseBetween(self, head, left, right):
-        """
-        :type head: Optional[ListNode]
-        :type left: int
-        :type right: int
-        :rtype: Optional[ListNode]
-        """
-        prev=None
-        curr=head
-        start=None
-        end=None
-        start_prev=None
-        end_post=None
-        while curr:
-            left-=1
-            right-=1
-            if left==0:
-                start=curr
-                start_prev=prev
-            if right==0:
-                end=curr
-                end_post=curr.next
-                break
-            prev=curr
-            curr=curr.next
-        curr=start
-        prev=None
-        while curr!=end_post:
-            nex=curr.next
-            curr.next=prev
-            prev=curr
-            curr=nex
-        if start_prev:
-            start_prev.next=end
-        else:
-            head=end
-        start.next=end_post
-        return head
+        dummy = ListNode(0, head)
+        prev = dummy
+        for _ in range(left - 1):
+            prev = prev.next          # prev = node just before `left`
+
+        curr = prev.next              # curr will always be the (unchanged) start of the sublist
+        for _ in range(right - left):
+            nex = curr.next
+            curr.next = nex.next
+            nex.next = prev.next
+            prev.next = nex
+
+        return dummy.next
