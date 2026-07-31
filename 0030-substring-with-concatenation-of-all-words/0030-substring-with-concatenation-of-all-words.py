e93100a6003304af66516ce740f200e8f3682477
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-07-31 14:54:14 +0530

    Time: 35 ms (55.41%), Space: 13.3 MB (23.91%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,42 +1,44 @@
 class Solution(object):
     def findSubstring(self, s, words):
-        """
-        :type s: str
-        :type words: List[str]
-        :rtype: List[int]
-        """
-        word_len=len(words[0])
-        num_words=len(words)
-        window_len=word_len*num_words
-        word_count=Counter(words)
-        if len(word_count) == 1:
-            target = words[0]
-            c = []
+        word_len = len(words[0])
+        num_words = len(words)
+        window_len = word_len * num_words
+        word_count = Counter(words)
+        n = len(s)
+        res = []
 
-            for i in range(len(s) - window_len + 1):
-                if s[i:i + window_len] == target * num_words:
-                    c.append(i)
+        for offset in range(word_len):
+            low = offset
+            window_count = Counter()
+            num_matched = 0  # chunks currently in window that belong to word_count
 
-            return c
-        window=[""]*num_words
-        low=0
-        res=[]
-        high=window_len
-        while high<=len(s):
-            idx=low
-            for i in range(num_words):
-                window[i]=s[idx:idx+word_len]
-                idx+=word_len
-            if Counter(window)==word_count:
-                res.append(low)
-            # if window[0] in words : 
-            #     if len(window)>1 and window[0]!=window[1]:
-            #         low+=word_len
-            #         high+=word_len
-            #     else:
-            #         low+=1
-            #         high+=1
-            # else:
-            low+=1
-            high+=1
+            # slide idx by word_len each step, starting at offset
+            for idx in range(offset, n - word_len + 1, word_len):
+                word = s[idx:idx + word_len]
+
+                if word in word_count:
+                    window_count[word] += 1
+                    num_matched += 1
+
+                    # window has too many chunks -> shrink from left
+                    while window_count[word] > word_count[word]:
+                        left_word = s[low:low + word_len]
+                        window_count[left_word] -= 1
+                        num_matched -= 1
+                        low += word_len
+
+                    # window has exactly num_words chunks -> check
+                    if num_matched == num_words:
+                        res.append(low)
+                        # shrink by one chunk from left to slide forward
+                        left_word = s[low:low + word_len]
+                        window_count[left_word] -= 1
+                        num_matched -= 1
+                        low += word_len
+                else:
+                    # invalid chunk -> whole window resets
+                    window_count.clear()
+                    num_matched = 0
+                    low = idx + word_len
+
         return res
