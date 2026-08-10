32407063e62c997f92c21f3c8bb2c21c75e04619
Om Bharadwaj <154073952+Poseidon994@users.noreply.github.com>
2026-08-10 18:43:15 +0530

    Time: 120 ms (21.51%), Space: 17 MB (9.6%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -8,15 +8,16 @@ class Solution(object):
         res=0
         zero=0
         one=0
+        f[0]=-1
         for i in range(len(nums)):
             if nums[i]==0:
                 zero+=1
             else:
                 one+=1
             diff=zero-one
-            if diff==0:
-                res=max(res, i+1)
-                continue
+            # if diff==0:
+            #     res=max(res, i+1)
+            #     continue
             if diff not in f:
                 f[diff]=i
             else:
